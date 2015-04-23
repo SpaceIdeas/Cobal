ca18b854d0482c1e8d72f800bfbac8f02c2b77db
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-04-23 09:17:52 +0200

    For å sjekke smarty

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
  * Date: 22/04/2015
  * Time: 17:40
  */
-
+include('libs/Smarty.class.php');
 echo "hello world<br/>";
 echo " This is a unix system<br/>";
 echo "Also Tennesi burger<br/>";
 echo "hello my name is macintosh<br/>";
+$smarty = new Smarty();
+$smarty->display('index.tpl');
