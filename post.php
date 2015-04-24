4a6d097ea21806734892b937077486e2271e5af0
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-04-24 16:04:04 +0200

    addUser - navn endret till registrerUser

--- post.php
+++ post.php
@@ -5,3 +5,16 @@
  * Date: 24.04.2015
  * Time: 09:00
  */
+require_once('config.php');
+require_once('libs/Smarty.class.php');
+require_once('db.php');
+$smarty = new Smarty();
+if ($_GET ['id']) {
+    $smarty->assign('db', $db);
+    $post = Post::getPost($db, ( int ) $_GET ['id']);
+    if (isset($post)) {
+        $smarty->assign('post', $post);
+        $smarty->assign('comments', $post->getComments());
+    }
+}
+$smarty->display('post.tpl');
---
+++ templates/comment.tpl
@@ -0,0 +1,8 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">Klokken {$comment->getTimeCreated} kommenterte $comment->getAuthor()</h4>
+    </div>
+    <div class="panel-body">
+        {$comment->getText()}
+    </div>
+</div>
