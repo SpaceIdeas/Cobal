51da2df34919dfee44472dd6338df90bf921bed9
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-04-26 18:56:13 +0200

    La til funksjonalitet for å legge til et inlegg. Ikke ferdig, men det funker.

---
+++ addPost.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Øystein
+ * Date: 26.04.2015
+ * Time: 17:27
+ */
+require_once('config.php');
+require_once('libs/Smarty.class.php');
+require_once('db.php');
+session_start();
+$smarty = new Smarty();
+$smarty->display('addPost.tpl');
+
+if (isset($_POST['btnAddPost']))
+{
+    $post = new Post();
+    $post->setText($_POST['txtPost']);
+    $post->setTitle($_POST['txtTitle']);
+    $post->setAuthorEmail($_SESSION['user']->getEmail());
+    $post->addToDB($db);
+}
