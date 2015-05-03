302322b413569ae6a35566e1cd6ce8a741c5c9ec
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-05-03 16:09:58 +0200

    Endret autolader til å ta høyde for smarty

--- addPost.php
+++ addPost.php
@@ -6,7 +6,6 @@
  * Time: 17:27
  */
 require_once('config.php');
-require_once('libs/Smarty.class.php');
 require_once('db.php');
 session_start();
 $smarty = new Smarty();
@@ -19,7 +18,7 @@ if (isset($_POST['btnAddPost'])) {
     $post->setTitle($_POST['txtTitle']);
     $post->setAuthorEmail($_SESSION['user']->getEmail());
 
-    // Inlegget ble lagt til i databasen
+    // Innlegget ble lagt til i databasen
     if ($post->addToDB($db)) {
         $postID = $db->lastInsertId();
         // Hvis en fil er valgt som vedlegg
