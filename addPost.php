58957e2024a98eb25b4f12efa55f1ebb3fa2ef59
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-05-03 18:16:29 +0200

    Fiksel feilmeldinger ved oprettelse av nytt innlegg...

--- addPost.php
+++ addPost.php
@@ -22,7 +22,7 @@ if (isset($_POST['btnAddPost'])) {
     if ($post->addToDB($db)) {
         $postID = $db->lastInsertId();
         // Hvis en fil er valgt som vedlegg
-        if (isset($_FILES['userfile']['tmp_name'])) {
+        if ($_FILES['userfile']['error'] != UPLOAD_ERR_NO_FILE) {
             // Henter ut ID til inlegget som nettop ble lagt til
             // Hvis filen som er laste opp er den som skulle lastes opp
             if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
@@ -44,12 +44,12 @@ if (isset($_POST['btnAddPost'])) {
             }
             // Finner ikke riktig fil på serveren
             else {
-                $alert = new Alert(Alert::ERROR, 'Filen som ble lastet opp er kanskje en del av et angrep. Kun inlegget ble lastet opp');
+                $alert = new Alert(Alert::ERROR, 'Filen som ble lastet opp er kanskje en del av et angrep. Kun innlegget ble lastet opp');
                 $alert->displayOnOtherPage('post.php?id=' . $postID);
             }
         }
         // Inlegget ble lagt til (vedlegg ikke valgt). Sender brukeren til det nye inlegget og gir han suksessmelding.
-        $alert = new Alert(Alert::SUCCESS, 'Ditt inlegg ble lagt til');
+        $alert = new Alert(Alert::SUCCESS, 'Ditt innlegg ble lagt til');
         $alert->displayOnOtherPage('post.php?id=' . $postID);
     }
     // Inlegget ble ikke lagt til i databasen på grunn av en feil
