c44a114bbae63ad355cb7e3d5fc74e8afacd790d
Øystein Danielsen <danielsen.oeystein@gmail.com>
2015-05-10 15:44:50 +0200

    Slettet tidligere innleveringer. kommenterte profileimage.php

--- oeystein/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Dette er hovedisden. Poster hentes og sendes til smarty */
-
-require_once 'config.php';
-$smarty = new Smarty();
-$articles = dbConnection::getArticles();
-$smarty->assign('articles', $articles);
-$smarty->display('index.tpl');
-?>
--- profileImage.php
+++ profileImage.php
@@ -4,32 +4,43 @@
  * User: Øystein
  * Date: 06.05.2015
  * Time: 14:20
+ *
+ * Dette scriptet er laget for å returnere profilbilder som kan brukes i en <img> tag.
+ * Meningen er at man skal kunne sende med id til comment eller post og få ut en link til profilbildet som ligger
+ * i databasen til forfatteren av posten eller kommentaren.
+
  */
-// Brukeren har valgt å vise vedlegg
 require_once('db.php');
 require_once('config.php');
+
+    // Profilbildet til forfatteren av en post skal hentes ut.
     if (isset($_GET['postID'])) {
 
         $post = POST::getPost($db, $_GET['postID']);
+        // Denne metoden returener et ProfileImage som samsvarer med forfatteren.
         $profileImage = ProfileImage::getProfileImage($db, $post->getAuthorEmail());
+        // ProfileImage er kun ikke null, hvis brukeren har et profilbilde.
         if (isset($profileImage)) {
             header("Content-Type:" . $profileImage->getMIMEType());
             echo($profileImage->getPicture());
         }
+        // Returnerer en link til deafult-profilbildet hvis et profilbilde ikke ble funnet
         else {
             header("Location: img/DefaultProfilePic.jpg");
             die();
         }
     }
-
+    // Profilbildet til forfatteren av en kommentar skal hentes ut.
     elseif(isset($_GET['commentID'])) {
             $comment = Comment::getCommentByID($db, $_GET['commentID']);
+            // Denne metoden returener et ProfileImage som samsvarer med forfatteren.
             $profileImage = ProfileImage::getProfileImage($db, $comment->getAuthorEmail());
-
+        // ProfileImage er kun ikke null, hvis brukeren har et profilbilde.
             if (isset($profileImage)) {
                 header("Content-Type:" . $profileImage->getMIMEType());
                 echo($profileImage->getPicture());
             }
+            // Returnerer en link til deafult-profilbildet hvis et profilbilde ikke ble funnet
             else {
                 header("Location: img/DefaultProfilePic.jpg");
                 die();
